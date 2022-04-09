1ab551dc55bbb72427d26c38985dc80061d7793f
fran <fran@penguin>
2022-04-09 17:05:21 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from scripts import random, plot, pdf, ex
-import matplotlib.pyplot as plt
+import scripts as src
 
-s = random.get_num()
-p = ex.type_10(s)
-
-plt.title(p["plot"], fontsize=p["font"], y=0.35)
-plt.axes().set_axis_off()
-plt.savefig("out.png")
+src.sql.connect()
--- scripts/sql/__init__.py
+++ scripts/sql/__init__.py
@@ -2,13 +2,13 @@ import psycopg2 as pg
 import dotenv
 import os
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+def connect():
 
-db = os.environ.get("POSTGRE_DB")
-user = os.environ.get("POSTGRE_USER")
-password = os.environ.get("POSTGRE_PASS")
-host = os.environ.get("POSTGRE_HOST")
-port = os.environ.get("POSTGRE_PORT")
+    db = os.environ.get("POSTGRE_DB")
+    user = os.environ.get("POSTGRE_USER")
+    password = os.environ.get("POSTGRE_PASS")
+    host = os.environ.get("POSTGRE_HOST")
+    port = os.environ.get("POSTGRE_PORT")
 
-conn = pg.connect(database=db, user=user, password=password, host=host, port=port)
-cursor = conn.cursor()
+    conn = pg.connect(database=db, user=user, password=password, host=host, port=port)
+    cursor = conn.cursor()
